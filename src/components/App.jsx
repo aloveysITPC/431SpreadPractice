c1887b0a315ae2663837c5b7a7a3e69233a0c528
Ali Loveys <92459028+aloveysITPC@users.noreply.github.com>
2022-07-25 11:59:19 -0400

    lessonWithNotes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  //create a stateful constant with initial state of blank
+  const [inputText, setInputText] = useState("");
+  //create an array for all items
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    //create a new constant that gets hold of the new entered value
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input
+          name="item"
+          type="text"
+          onChange={handleChange}
+          value={inputText}
+        />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {/* {items.map((todoItem) => {
+            return <li>{todoItem}</li>;
+          })} */}
+          {/* //simplify the code */}
+          {/* map through all of the items 
+          for each to-do item, create an li and 
+          put the to-do item in that li */}
+          {items.map((todoItem) => (
+            <li>{todoItem}</li>
+          ))}
         </ul>
       </div>
     </div>
